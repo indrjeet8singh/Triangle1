15c616bfe18c6f00379738419d9c096ed9bda6be
Welcome <Welcome@DESKTOP-U43JIC9>
2022-02-19 22:01:43 +0530

    Add project files.

---
+++ Triangle1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Triangle1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         for(int i=1; i<=6; i++)
+            {
+               Console.Write(" ");
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
